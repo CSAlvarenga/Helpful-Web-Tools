5d5dcfe736bbdd03d198e748213fee53a6d55d8c
CSAlvarenga <csantosalvarenga@gmail.com>
2026-04-27 21:13:54 -0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,4 +16,16 @@ export default defineConfig({
   build: {
     outDir: 'dist',
   },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.js$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
 })
